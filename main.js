edb3be2303b49d4a9be83bceec6f5130db40f616
Garrett Richards <garrettxaivier@gmail.com>
2018-12-12 16:12:51 -0800

    etsy search page

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 	$("#sect2, #sect3").hide()
+	$("#tabs li:nth-child(1)").addClass('active')
 
-	$("#tabs").on('click', 'li', function(){
-
+	$("#tabs").on('click', 'li', function(a){
+		
 	})
 })
